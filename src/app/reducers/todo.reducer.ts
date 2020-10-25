f2c6cb2cf2c827ea6c0d591fb03be86b913617c2
renzivan <renzivanyapenguio@gmail.com>
2020-10-25 15:09:11 +0800

    added todo item interface

--- src/app/reducers/todo.reducer.ts
+++ src/app/reducers/todo.reducer.ts
@@ -1,19 +1,19 @@
 import { createReducer, on } from '@ngrx/store'
 import { addTodo, deleteTodo } from '../actions/todo.actions'
+import { TodoItem } from '../interfaces/todo-item'
 
 export const initialState: any[] = []
 let id: number = 0
+let todoItem: TodoItem
 
 const _todoReducer = createReducer(
   initialState,
   on(addTodo, (state: any, { newTodo }) => {
-    return [
-      ...state,
-      {
-        id: id++,
-        title: newTodo
-      }
-    ]
+    todoItem = {
+      id: id++,
+      title: newTodo
+    }
+    return [...state, todoItem]
   }),
   on(deleteTodo, (state: any, { todoId }) => {
     const todos = [...state].filter(item => {
